1bc77bb6984262f697f4ad57b6c0a44dda7e3928
Danilo Costa <dandan_costa2222@outlook.com>
2025-05-13 16:07:48 -0300

    codigo do exercicio

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿double salariohoraBase, salario;
+int horas, horasExtras;
+
+Console.WriteLine("--- Horas Extras ---\n");
+
+Console.Write("Digite o salario-hora base: ");
+salariohoraBase = Convert.ToDouble(Console.ReadLine());
+Console.Write("Digite as horas trabalhadadas: ");
+horas = Convert.ToInt32(Console.ReadLine());
+Console.Write("Digite a quantidade de horas extras: ");
+horasExtras = Convert.ToInt32(Console.ReadLine());
+
+salario = salariofinal();
+
+Console.WriteLine($"O salario é {salario:C2} por hora");
+
+double salariofinal(){
+    double salarionormal = salariohoraBase * horas;
+    double salarioHorasExtras = salariohoraBase * horasExtras * 1.4;
+    return salarionormal + salarioHorasExtras;
+}
